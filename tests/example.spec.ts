8a8692739ee8d19628752b73fe8a8cb605c54216
Ana Prpic <ana.prpic@five.agency>
2024-02-08 11:24:09 +0100

    add some tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,27 @@
-import { test, expect } from '@playwright/test';
+import {
+  test,
+  expect,
+} from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/index.html');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Demo Test', () => {
+  test('check url', async ({ page }) => {
+    await expect(page.url().includes('/index.html')).toBeTruthy();
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  test('has title', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/STORE/);
+  });
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  test('cart link', async ({ page }) => {
+    // Click the cart link.
+    await page.getByRole('link', { name: 'Cart' }).click();
+
+    // Expects page to have a heading with the name of Products.
+    await expect(page.getByRole('heading', { name: 'Products' })).toBeVisible();
+  });
 });
